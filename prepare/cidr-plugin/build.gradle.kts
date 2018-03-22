07afeee7c4a7c88e45bce47a7ca42173184b3254
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-03-22 23:37:24 +0300

    Build: Add :prepare:cidr-plugin project

---
+++ prepare/cidr-plugin/build.gradle.kts
@@ -0,0 +1,55 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import groovy.lang.Closure
+import java.io.FilterReader
+
+description = "Kotlin AppCode & CLion plugin"
+
+apply {
+    plugin("kotlin")
+}
+
+val ideaPluginDir: File by rootProject.extra
+val cidrPluginDir: File by rootProject.extra
+
+val kotlinPlugin by configurations.creating
+
+val pluginXmlPath = "META-INF/plugin.xml"
+
+dependencies {
+    kotlinPlugin(project(":prepare:idea-plugin", configuration = "runtimeJar"))
+}
+
+val pluginXml by tasks.creating {
+    val placeholderRegex = """\<\!\-\- CLION\-PLUGIN\-PLACEHOLDER\-START \-\-\>(.*)\<\!\-\- CLION\-PLUGIN\-PLACEHOLDER\-END \-\-\>"""
+
+    inputs.files(kotlinPlugin)
+    outputs.files(File(buildDir, name, pluginXmlPath))
+
+    doFirst {
+        zipTree(inputs.files.singleFile)
+            .matching { include(pluginXmlPath) }
+            .singleFile
+            .readText()
+            .replace(Regex(placeholderRegex, RegexOption.DOT_MATCHES_ALL), "")
+            .also { pluginXmlText ->
+                outputs.files.singleFile.writeText(pluginXmlText)
+            }
+    }
+}
+
+val jar = runtimeJar {
+    archiveName = "kotlin-plugin.jar"
+    dependsOn(kotlinPlugin)
+    from {
+        zipTree(kotlinPlugin.singleFile).matching {
+            exclude(pluginXmlPath)
+        }
+    }
+    from(pluginXml) { into("META-INF") }
+}
+
+task<Copy>("cidrPlugin") {
+    into(cidrPluginDir)
+    from(ideaPluginDir) { exclude("lib/kotlin-plugin.jar") }
+    from(jar) { into("lib") }
+}
